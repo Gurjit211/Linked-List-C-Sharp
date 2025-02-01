287136fdd60b3743c903aedd953a373a55aff634
Gurjit211 <165533179+Gurjit211@users.noreply.github.com>
2025-02-01 12:17:06 -0500

    Add files via upload

---
+++ SinglyLinkedList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedList
+{
+     class Program
+    {
+        static void Main()
+        {
+            SinglyLinkedList<int> myLL = new SinglyLinkedList<int>(); //here myLL means myLinkedList
+            myLL.AddFirst(11);
+            myLL.AddLast(06);
+            myLL.AddFirst(93);
+            myLL.RemoveLast();
+            myLL.RemoveFirst();
+            myLL.AddFirst(44);
+            myLL.AddLast(295);
+            myLL.AddLast(5911);
+            myLL.AddFirst(47);
+            myLL.AddFirst(26);
+            myLL.RemoveLast();
+   
+            Node<int> current = myLL.HeadNode;
+            while (current != null)
+            {
+                Console.WriteLine(current.NodeData);
+                current = current.NextNode;
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedList
+{
+    public class SinglyLinkedList<T>
+    {
+        private Node<T> headNode;
+        private Node<T> tailNode;
+        private int count;
+
+        public Node<T> HeadNode => headNode;
+        public Node<T> TailNode => tailNode;
+        public int Count => count;
+        public void AddFirst(T nodeData)
+        {
+            Node<T> node = new Node<T>(nodeData);
+            node.NextNode = headNode;
+            headNode = node;
+
+            if (count == 0)
+            {
+                tailNode = headNode;
+            }
+
+            count++;
+        }
+
+        public void AddLast(T nodeData)
+        {
+            Node<T> node = new Node<T>(nodeData);
+            if (count == 0)
+            {
+                headNode = node;
+            }
+            else
+            {
+                tailNode.NextNode = node;
+            }
+            tailNode = node;
+            count++;
+        }
+
+        public void RemoveFirst()
+        {
+            if (count != 0)
+            {
+                headNode = headNode.NextNode;
+                count--;
+                if (count == 0)
+                {
+                    tailNode = null;
+                }
+            }
+        }
+
+        public void RemoveLast()
+        {
+            if (count != 0)
+            {
+                if (count == 1)
+                {
+                    headNode = null;
+                    tailNode = null;
+                }
+                else
+                {
+                    Node<T> current = headNode;
+                    while (current.NextNode != tailNode)
+                    {
+                        current = current.NextNode;
+                    }
+                    current.NextNode = null;
+                    tailNode = current;
+                }
+                count--;
+            }
+        }
+
+        
+    } // class ends here
+}// namespace ends here
